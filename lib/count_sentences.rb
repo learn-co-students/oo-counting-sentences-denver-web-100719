837c42dfb474cbb62b67dc226a0d3d307487aba0
ParkerBacall <pbacall2@gmail.com>
2019-11-22 13:40:48 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    self.split("?".. ".".. "!")
+    self.length
   end
 end
